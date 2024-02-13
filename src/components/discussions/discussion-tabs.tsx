e94dd17ae8652ad98b3d82956a5ce483498b6aba
sukrit101 <sukrit@m0.ventures>
2024-02-13 12:19:54 +0530

    enhance: switching to link tags for discussion tab change

--- src/components/discussions/discussion-tabs.tsx
+++ src/components/discussions/discussion-tabs.tsx
@@ -1,5 +1,4 @@
-import qs from "query-string";
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import {
   Hash,
@@ -7,6 +6,7 @@ import {
   Megaphone,
   MessageCircleQuestion,
 } from "lucide-react";
+import Link from "next/link";
 import { useAtom } from "jotai";
 
 import { cn } from "@/lib/utils";
@@ -47,39 +47,13 @@ export const DiscussionTabs = ({ classroomId }: { classroomId: string }) => {
     setIsChangingQuery(false);
   }, [params]);
 
-  const handleQueryChange = useCallback(
-    (value: string) => {
-      let currentQuery = {};
-
-      if (params) {
-        currentQuery = qs.parse(params.toString());
-      }
-
-      const updatedQuery: any = {
-        ...currentQuery,
-        tab: value,
-        active: undefined,
-      };
-
-      const url = qs.stringifyUrl(
-        {
-          url: `/c/${classroomId}`,
-          query: updatedQuery,
-        },
-        { skipNull: true }
-      );
-
-      router.push(url);
-    },
-    [params]
-  );
-
   return (
     <div className="space-y-4 text-neutral-800">
       <h3 className="tracking-tight font-medium text-[13px]">Categories</h3>
       <div className="flex flex-col gap-y-2">
         {tabs.map((tab, index) => (
-          <div
+          <Link
+            href={`/c/${classroomId}?tab=${tab.value}`}
             key={index}
             className={cn(
               "py-1 px-2.5 flex items-center gap-x-2 hover:bg-neutral-100 rounded-md text-[13px] cursor-pointer",
@@ -89,12 +63,11 @@ export const DiscussionTabs = ({ classroomId }: { classroomId: string }) => {
             )}
             onClick={() => {
               setIsChangingQuery(true);
-              handleQueryChange(tab.value);
             }}
           >
             <tab.icon className="h-3.5 w-3.5" />
             <p>{tab.label}</p>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
