e718364de32692911c09de330805e8891c37483d
sukrit101 <sukrit@m0.ventures>
2024-03-26 14:36:17 +0530

    test: event date testing

--- src/components/event/events.tsx
+++ src/components/event/events.tsx
@@ -25,6 +25,8 @@ export const Events: React.FC<EventsProps> = ({ date }) => {
   const [overDate, setOverDate] = useAtom(overDateAtom);
   const [activeDateObj, setActiveDateObj] = useAtom(activeDateAtom);
 
+  console.log("client date", date);
+
   const { data: serverEvents, isLoading } = useGetUpcomingEvents(
     undefined,
     date
