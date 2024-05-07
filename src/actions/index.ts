a3bcf4c1b41f031d65e481208f7bbf46f1577313
sukrit101 <sukrit@m0.ventures>
2024-05-07 20:50:46 +0530

    testing dates

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,14 +4,14 @@ import { add } from "date-fns";
 import { serverClient } from "@/trpc/server-client";
 
 export const getUpcomingEvents = async (date: Date) => {
-  const updatedDate = add(date, {
-    hours: 5,
-    minutes: 30,
-  });
+  //   const updatedDate = add(date, {
+  //     hours: 5,
+  //     minutes: 30,
+  //   });
 
   const events = await serverClient.event.getEvents({
     classroomId: undefined,
-    date: updatedDate,
+    date,
   });
 
   return events;
--- src/server/event/routes.ts
+++ src/server/event/routes.ts
@@ -71,11 +71,27 @@ export const getEvents = privateProcedure
       console.log("UPDATED API DATE", format(date, "MMMM do, h:mm a"));
       console.log(
         "START UPDATED API DATE",
-        format(startOfDay(date), "MMMM do, h:mm a")
+        format(
+          startOfDay(
+            add(date, {
+              hours: 5,
+              minutes: 30,
+            })
+          ),
+          "MMMM do, h:mm a"
+        )
       );
       console.log(
         "END UPDATED API DATE",
-        format(endOfDay(date), "MMMM do, h:mm a")
+        format(
+          endOfDay(
+            add(date, {
+              hours: 5,
+              minutes: 30,
+            })
+          ),
+          "MMMM do, h:mm a"
+        )
       );
 
       // const indianTimeZone = new Date(date).toLocaleString("en-US", {
@@ -85,8 +101,18 @@ export const getEvents = privateProcedure
       // const indianDate = new Date(indianTimeZone);
 
       eventDateWhereClause = {
-        gte: startOfDay(date),
-        lte: endOfDay(date),
+        gte: startOfDay(
+          add(date, {
+            hours: 5,
+            minutes: 30,
+          })
+        ),
+        lte: endOfDay(
+          add(date, {
+            hours: 5,
+            minutes: 30,
+          })
+        ),
       };
     } else {
       eventDateWhereClause = {
