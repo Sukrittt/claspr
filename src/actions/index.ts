2c1eb415a3ccd84e681ab5bace4430a9fa84a5e3
sukrit101 <sukrit@m0.ventures>
2024-05-07 16:17:00 +0530

    adding console logs

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,9 +3,14 @@
 import { serverClient } from "@/trpc/server-client";
 
 export const getUpcomingEvents = async (date: Date) => {
+  const serverDate = new Date(date);
+
+  console.log("CLIENT DATE", format("MMMM do, h:mm a", date));
+  console.log("SERVER DATE", format("MMMM do, h:mm a", serverDate));
+
   const events = await serverClient.event.getEvents({
     classroomId: undefined,
-    date,
+    date: serverDate,
   });
 
   return events;
