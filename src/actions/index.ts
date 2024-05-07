38679f7a19dc32fb07625c2156fef803e909c27c
sukrit101 <sukrit@m0.ventures>
2024-05-07 21:19:04 +0530

    testing dates

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,16 +1,10 @@
 "use server";
-import { add } from "date-fns";
-
 import { serverClient } from "@/trpc/server-client";
 
 export const getUpcomingEvents = async (date: Date) => {
-  const updatedDate = add(date, {
-    days: 1,
-  });
-
   const events = await serverClient.event.getEvents({
     classroomId: undefined,
-    date: updatedDate,
+    date,
   });
 
   return events;
